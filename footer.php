14e4c6a580c6ec8ae9f5f990767cb444c6579fb0
Appie Verschoor <xiffy@xiffy.nl>
2013-12-04 00:20:11 +0100

    Footertekst aangepast

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 ﻿<footer id="footer">
-	<p><strong>nrctweet-count-bot: gebaseerd op dcrrspndt van Appie Verschoor</strong><br/>
-	sinds 27 oktober 2013 struint de bot twitter af om alle links naar nrc.nl te tellen. Sinds 15.00 uur is de auteur en de categorie ook goed ingevuld, oudere links worden hersteld als ze nog eens op twitter langskomen.</p>
-	<p>sind 29 oktober wordt ook de publicatiedatum van een artikel bepaald, deze zijn herkenbaar aan een geschreven maandnaam ipv een nummer in de tabel. Eerder gevonden artikelen worden hersteld als er nog een tweet uitgaat voor die artikelen, in alle andere gevallen zie je de tijd en datum waarop de twitterbot het artikel voor het eerst zag.</p>
-	<p>de code van dit project vind je op github: <a href="http://github.com/nrcmedia/dcrrspndnt/">project: dcrrspndnt</a>. Patches en pull requests zijn welkom.</p>
+	<p><strong>vktweet-count-bot: gebaseerd op dcrrspndt van Appie Verschoor</strong><br/>
+	sinds 04 december 2013 struint de bot twitter af om alle links naar vk.nl te tellen.</p>
+	<p>de code van dit project vind je op github: <a href="http://github.com/nrcmedia/vktweets/">project: vktweets</a>. Patches en pull requests zijn welkom.</p>
 	<p><strong>buy me a beer: 19nUw7xTwZ5EzTqcJzwg6pavRerUD8Mx7F (bitcoin)</strong></p>
 </footer>
